bc56a64ea6a60f2ca8bea8f6ff9e91ece3da189d
Ivar Holck <ivar.holck@gmail.com>
2017-10-17 18:09:39 +0200

    Added server.js, modified Readme.md

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # ns-js
+
+to clone: 'git clone https://github.com/iholck/ns-js.git'
+
+Needs node/npm
+Express: ('npm install express --save')
+
+To start: npm server.js
---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+app.get('/', function (request,response){
+	response.send('Hello World');
+});
+
+const port = 3000;
+app.listen(port,() => {
+	console.log(`Example app listening on port ${port}`);
+
+})
